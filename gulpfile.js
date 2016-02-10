01b35bada808aed364972b4e1dc2458a5dff3ebd
Gabi <gabrielahess.1@gmail.com>
2016-02-11 00:05:56 +0100

    version PC

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
 var connect = require('gulp-connect');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('webserver', function () {
     connect.server({
@@ -31,11 +33,21 @@ gulp.task('pure_reload', function () {
 
 gulp.task('watch', function () {
     gulp.watch(['templates/**/*.html', 'templates/*.html'], ['reload']);
-    gulp.watch(['styles/main.css','media/images/*'], ['pure_reload']);
+    gulp.watch(['media/images/*'], ['pure_reload']);
+    gulp.watch(['styles/*.scss'], ['sass']);
 
 });
 
+gulp.task('sass', function() {
+  return gulp.src('styles/main.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('styles/'))
+      .pipe(connect.reload());
+});
 
-gulp.task('default', ['nunjucks', 'watch', 'webserver'], function () {
+
+gulp.task('default', ['nunjucks', 'watch', 'webserver', 'sass'], function () {
 
 });
